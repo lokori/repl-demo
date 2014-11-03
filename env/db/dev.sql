f07303b1fee41a8236618a04c0ee47eabab3eb60
Antti Virtanen <antti.virtanen@iki.fi>
2014-11-03 22:11:38 +0200

    Väliviiva vaihdettu alaviivaan. Alaviiva ei kelvannut Postgrelle

--- env/db/dev.sql
+++ env/db/dev.sql
@@ -1,6 +1,6 @@
-create user repl-adm with password 'repl-adm';
+create user repl_adm with password 'repl-adm';
 CREATE DATABASE sourcedb;
 CREATE DATABASE targetdb;
-GRANT ALL PRIVILEGES ON DATABASE sourcedb to repl-adm;
-GRANT ALL PRIVILEGES ON DATABASE targetdb to repl-adm;
+GRANT ALL PRIVILEGES ON DATABASE sourcedb to repl_adm;
+GRANT ALL PRIVILEGES ON DATABASE targetdb to repl_adm;
 
